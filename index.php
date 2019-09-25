dd476d63faacc62fcf164f09ba6dabe8d11da5db
MaxiTec <mtec@oasishoteles.com>
2019-09-25 16:34:03 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,74 @@
+
+<h1>TEST HTTP2 :)</h1>
+<p>
+<?php echo('lorem ipsum :)');?></p>
+
+<div class="grid-hoteles wrap-container text-center">
+    <div class="row grid-hoteles__slick gallery-unslick">
+        <div class="col-xs-12 col-sm-4 grid-hoteles__slick__item">
+            <a href="https://oasishoteles.com/es/hoteles/the-pyramid-at-grand-oasis" title="The Pyramid at Grand Cancun" class="grid-hoteles__item">
+                <div class="grid-hoteles__inner">
+                    <div class="grid-hoteles__img"> <img class="lazyimg b-loaded" alt="Vista de hotel The Pyramid at Grand Cancun" title="Vista de hotel The Pyramid at Grand Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/hoteles/the-pyramid-at-grand-oasis/the-pyramid-at-grand-oasis-thumb.png"> </div> <img data-eventcategory="Inicio" data-eventaction="Hoteles" data-eventlabel="The Pyramid at Grand Cancun" data-eventtrigger="clicGeneral" class="lazyimg b-loaded" alt="Vista de hotel The Pyramid at Grand Cancun" title="Vista de hotel The Pyramid at Grand Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/home/portada-hoteles/pyr_thumb.jpg">
+                    <div class="grid-hoteles__name">
+                        <div class="grid-hoteles__button">
+                            <h3> <span>The Pyramid at Grand Cancun</span> </h3> </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-12 col-sm-4 grid-hoteles__slick__item">
+            <a href="https://oasishoteles.com/es/hoteles/the-sian-ka-an-at-grand-sens" title="The Sian Ka'an at Sens Cancun" class="grid-hoteles__item">
+                <div class="grid-hoteles__inner">
+                    <div class="grid-hoteles__img"> <img class="lazyimg b-loaded" alt="Vista de hotel The Sian Ka'an at Sens Cancun" title="Vista de hotel The Sian Ka'an at Sens Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/hoteles/the-sian-ka-an-at-sens-cancun/sks-thumb.png"> </div> <img data-eventcategory="Inicio" data-eventaction="Hoteles" data-eventlabel="The Sian Ka'an at Sens Cancun" data-eventtrigger="clicGeneral" class="lazyimg b-loaded" alt="Vista de hotel The Sian Ka'an at Sens Cancun" title="Vista de hotel The Sian Ka'an at Sens Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/home/portada-hoteles/tsks_thumb.jpg">
+                    <div class="grid-hoteles__name">
+                        <div class="grid-hoteles__button">
+                            <h3> <span>The Sian Ka'an at Sens Cancun</span> </h3> </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-12 col-sm-4 grid-hoteles__slick__item">
+            <a href="https://oasishoteles.com/es/hoteles/grand-oasis-cancun" title="Grand Oasis Cancun" class="grid-hoteles__item">
+                <div class="grid-hoteles__inner">
+                    <div class="grid-hoteles__img"> <img class="lazyimg b-loaded" alt="Vista de hotel Grand Oasis Cancun" title="Vista de hotel Grand Oasis Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/hoteles/grand-oasis-cancun/grand-oasis-cancun-thumb.png"> </div> <img data-eventcategory="Inicio" data-eventaction="Hoteles" data-eventlabel="Grand Oasis Cancun" data-eventtrigger="clicGeneral" class="lazyimg b-loaded" alt="Vista de hotel Grand Oasis Cancun" title="Vista de hotel Grand Oasis Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/home/portada-hoteles/goc_thumb.jpg">
+                    <div class="grid-hoteles__name">
+                        <div class="grid-hoteles__button">
+                            <h3> <span>Grand Oasis Cancun</span> </h3> </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-12 col-sm-4 grid-hoteles__slick__item">
+            <a href="https://oasishoteles.com/es/hoteles/grand-oasis-sens" title="Grand Sens Cancun" class="grid-hoteles__item">
+                <div class="grid-hoteles__inner">
+                    <div class="grid-hoteles__img"> <img class="lazyimg b-loaded" alt="Vista de hotel Grand Sens Cancun" title="Vista de hotel Grand Sens Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/hoteles/grand-sens-cancun/grand-sens-cancun-thumb.png"> </div> <img data-eventcategory="Inicio" data-eventaction="Hoteles" data-eventlabel="Grand Sens Cancun" data-eventtrigger="clicGeneral" class="lazyimg b-loaded" alt="Vista de hotel Grand Sens Cancun" title="Vista de hotel Grand Sens Cancun" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/home/portada-hoteles/gsc_thumb.jpg">
+                    <div class="grid-hoteles__name">
+                        <div class="grid-hoteles__button">
+                            <h3> <span>Grand Sens Cancun</span> </h3> </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-12 col-sm-4 grid-hoteles__slick__item">
+            <a href="https://oasishoteles.com/es/hoteles/grand-oasis-palm" title="Grand Oasis Palm" class="grid-hoteles__item">
+                <div class="grid-hoteles__inner">
+                    <div class="grid-hoteles__img"> <img class="lazyimg b-loaded" alt="Vista de hotel Grand Oasis Palm" title="Vista de hotel Grand Oasis Palm" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/hoteles/grand-oasis-palm/grand-oasis-palm-thumb.png"> </div> <img data-eventcategory="Inicio" data-eventaction="Hoteles" data-eventlabel="Grand Oasis Palm" data-eventtrigger="clicGeneral" class="lazyimg b-loaded" alt="Vista de hotel Grand Oasis Palm" title="Vista de hotel Grand Oasis Palm" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/home/portada-hoteles/gop_thumb.jpg">
+                    <div class="grid-hoteles__name">
+                        <div class="grid-hoteles__button">
+                            <h3> <span>Grand Oasis Palm</span> </h3> </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-xs-12 col-sm-4 grid-hoteles__slick__item">
+            <a href="https://oasishoteles.com/es/hoteles/oh-cancun" title="Oh! Cancun The Urban Oasis" class="grid-hoteles__item">
+                <div class="grid-hoteles__inner">
+                    <div class="grid-hoteles__img"> <img class="lazyimg b-loaded" alt="Vista de hotel Oh! Cancun The Urban Oasis" title="Vista de hotel Oh! Cancun The Urban Oasis" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/hoteles/oh-the-urban-oasis/oh-the-urban-oasis-thumb.png"> </div> <img data-eventcategory="Inicio" data-eventaction="Hoteles" data-eventlabel="Oh! Cancun The Urban Oasis" data-eventtrigger="clicGeneral" class="lazyimg b-loaded" alt="Vista de hotel Oh! Cancun The Urban Oasis" title="Vista de hotel Oh! Cancun The Urban Oasis" src="https://oasishoteles.sfo2.cdn.digitaloceanspaces.com/assets/img/home/portada-hoteles/oh_thumb.jpg">
+                    <div class="grid-hoteles__name">
+                        <div class="grid-hoteles__button">
+                            <h3> <span>Oh! Cancun The Urban Oasis</span> </h3> </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div> <a data-eventcategory="Inicio" data-eventaction="Hoteles" data-eventlabel="VER MAS HOTELES" data-eventtrigger="clicGeneral" href="https://oasishoteles.com/es/hoteles/cancun" class="btn btn-gray-soft grid-hoteles__ver-mas" title="Hoteles en Cancun">ver más hoteles</a> </div>
